17b25dc70c531e3a2527daa46a5b41bc228e8cd6
Fabricio Leite <fabricio_2310@hotmail.com>
2024-09-12 22:03:07 +0100

    (Update) init ex00

---
+++ src/ex00/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faaraujo <faaraujo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 19:26:45 by faaraujo          #+#    #+#             */
+/*   Updated: 2024/09/12 19:41:05 by faaraujo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Você precisa criar um programa que emita o valor de uma certa quantia de 
+bitcoin em uma certa data.
+
+Este programa deve usar um banco de dados no formato csv que representará o preço 
+do bitcoin ao longo do tempo. Este banco de dados é fornecido com este assunto.
+
+O programa tomará como entrada um segundo banco de dados, armazenando os 
+diferentes preços/datas para avaliar.
+
+Seu programa deve respeitar estas regras:
+- O nome do programa é btc.
+- Seu programa deve tomar um arquivo como argumento.
+- Cada linha neste arquivo deve usar o seguinte formato: "data | valor".
+- Uma data válida sempre estará no seguinte formato: Ano-Mês-Dia.
+- ​​Um valor válido deve ser um float ou um inteiro positivo, entre 0 e 1000.
+
+Você deve usar pelo menos um contêiner em seu código para validar este
+exercício. 
+Você deve lidar com possíveis erros com uma mensagem de erro apropriada.
+
+Seu programa usará o valor em seu arquivo de entrada.
+
+Seu programa deve exibir na saída padrão o resultado do valor multiplicado pela 
+taxa de câmbio de acordo com a data indicada em seu banco de dados.
+
+Se a data usada na entrada não existir em seu BD, então você deve usar a 
+data mais próxima contida em seu BD. Tenha cuidado para usar a data mais 
+baixa e não a mais alta.
+*/
---
+++ src/ex01/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faaraujo <faaraujo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 19:31:45 by faaraujo          #+#    #+#             */
+/*   Updated: 2024/09/12 19:40:47 by faaraujo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Você deve criar um programa com estas restrições:
+- O nome do programa é RPN.
+- Seu programa deve ter uma expressão matemática polonesa invertida como argumento.
+- Os números usados ​​nesta operação e passados ​​como argumentos serão sempre menores 
+que 10. O cálculo em si, mas também o resultado, não levam em conta esta regra.
+- Seu programa deve processar esta expressão e gerar o resultado correto na saída padrão.
+- Se ocorrer um erro durante a execução do programa, uma mensagem de erro 
+deve ser exibida no erro padrão.
+- Seu programa deve ser capaz de manipular operações com estes tokens: "+ - / *".
+
+Você deve usar pelo menos um contêiner em seu código para validar este
+exercício.
+
+Você não precisa gerenciar os colchetes ou números decimais.
+*/
